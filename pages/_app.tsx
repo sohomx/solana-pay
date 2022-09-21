8a2f66d4b2c0712f958001443ced0b97d7841b9d
Sohom Pal <sohom@Sohoms-MacBook-Air.local>
2022-09-21 11:11:38 +0530

    add solana wallet button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,44 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { clusterApiUrl } from '@solana/web3.js'
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
+
+// default styles
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet
+
+  const endpoint = clusterApiUrl(network)
+
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter({ network }),
+  ]
+
   return (
-    <Layout>
-      <Head>
-        <title>Cookies Inc</title>
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Layout>
+            <Head>
+              <title>Cookies Inc</title>
+            </Head>
+            <Component {...pageProps} />
+          </Layout>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import Products from '../components/Products'
 import SiteHeading from '../components/SiteHeading'
 
 export default function HomePage() {
+  const { publicKey } = useWallet()
+
   return (
-    <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
+    <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8 pt-24">
       <SiteHeading>Cookies Inc</SiteHeading>
 
-      <Products submitTarget='/checkout' enabled={true} />
+      {/* adding solana wallet connect button */}
+      <div className="basis-1/4">
+        <WalletMultiButton className="!bg-gray-900 hover:scale-105" />
+      </div>
+
+      {/* We disable checking out without a connected wallet */}
+      <Products submitTarget="/checkout" enabled={publicKey !== null} />
     </div>
   )
 }

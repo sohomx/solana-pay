03d45f25285c5325c6e99bc9d454aeb3f8aa824e
Sohom Pal <sohom@Sohoms-MacBook-Air.local>
2022-09-22 19:24:46 +0530

    requesting the transaction

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,18 +1,112 @@
-import { useRouter } from "next/router";
-import BackLink from "../components/BackLink";
-import PageHeading from "../components/PageHeading";
-import calculatePrice from "../lib/calculatePrice";
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { Keypair, Transaction } from '@solana/web3.js'
+import { useRouter } from 'next/router'
+import { useEffect, useMemo, useState } from 'react'
+import BackLink from '../components/BackLink'
+import Loading from '../components/Loading'
+import {
+  MakeTransactionInputData,
+  MakeTransactionOutputData,
+} from './api/makeTransaction'
 
 export default function Checkout() {
   const router = useRouter()
+  const { publicKey } = useWallet()
 
-  const amount = calculatePrice(router.query)
+  // state to hold API response fields
+  const [transaction, setTransaction] = useState<Transaction | null>(null)
+  const [message, setMessage] = useState<string | null>(null)
+
+  // reading the URL query (which includes our chosen products)
+  const searchParams = new URLSearchParams()
+  for (const [key, value] of Object.entries(router.query)) {
+    if (value) {
+      if (Array.isArray(value)) {
+        for (const v of value) {
+          searchParams.append(key, v)
+        }
+      } else {
+        searchParams.append(key, value)
+      }
+    }
+  }
+
+  // generate the unique reference which will be used for this transaction
+  const reference = useMemo(() => Keypair.generate().publicKey, [])
+
+  // Add it to the params we'll pass to the API
+  searchParams.append('reference', reference.toString())
+
+  // Use our API to fetch the transaction for the selected items
+  async function getTransaction() {
+    if (!publicKey) {
+      return
+    }
+
+    const body: MakeTransactionInputData = {
+      account: publicKey.toString(),
+    }
+
+    const response = await fetch(
+      `/api/makeTransaction?${searchParams.toString()}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    )
+
+    const json = (await response.json()) as MakeTransactionOutputData
+
+    if (response.status !== 200) {
+      console.error(json)
+    }
+
+    // Deserialize the transaction from the response
+    const transaction = Transaction.from(
+      Buffer.from(json.transaction, 'base64')
+    )
+    setTransaction(transaction)
+    setMessage(json.message)
+    console.log(transaction)
+  }
+
+  useEffect(() => {
+    getTransaction()
+  }, [publicKey])
+
+  if (!publicKey) {
+    return (
+      <div className="flex flex-col items-center gap-8">
+        <div>
+          <BackLink href="/">Cancel</BackLink>
+        </div>
+
+        <WalletMultiButton />
+
+        <p>You need to connect your wallet to make transactions</p>
+      </div>
+    )
+  }
 
   return (
-    <div className="flex flex-col gap-8 items-center">
-      <BackLink href='/'>Cancel</BackLink>
+    <div className="flex flex-col items-center gap-8">
+      <div>
+        <BackLink href="/">Cancel</BackLink>
+      </div>
 
-      <PageHeading>Checkout {amount.toString()} SOL</PageHeading>
+      <WalletMultiButton />
+
+      {message ? (
+        <p>{message} Please approve the transaction using your wallet</p>
+      ) : (
+        <p>
+          Creating transaction... <Loading />
+        </p>
+      )}
     </div>
   )
 }
